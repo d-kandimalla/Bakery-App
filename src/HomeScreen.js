c6f23209e80e547dbe30b9f012e347b0df659598
Dileep <dileep335151@gmail.com>
2023-12-02 22:19:07 +0530

    changes

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,15 +1,89 @@
 import { Box } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function HomeScreen() {
+  const colorList = ["Yellow", "Green", "Red"];
+  const [currentColor, setCurrentColor] = useState("Red");
+
+  useEffect(() => {
+    setInterval(() => {
+      setCurrentColor(
+        colorList.indexOf(currentColor) + 1 === 3
+          ? "Yellow"
+          : colorList[colorList.indexOf(currentColor) + 1]
+      );
+    }, 5000);
+  });
+
   return (
     <Box
       sx={{
         display: "flex",
         justifyContent: "center",
+        height: "100%",
+        width: "100%",
+        border: "1px solid black",
+        p: 1,
       }}
     >
-      HomeScreen
+      <Box
+        sx={
+          currentColor === "Red"
+            ? {
+                border: "1px solid gray",
+                borderRadius: "25px",
+                width: "20px",
+                height: "20px",
+                mr: 1,
+                backgroundColor: "red",
+              }
+            : {
+                border: "1px solid gray",
+                borderRadius: "25px",
+                width: "20px",
+                height: "20px",
+                mr: 1,
+              }
+        }
+      ></Box>
+      <Box
+        sx={
+          currentColor === "Yellow"
+            ? {
+                border: "1px solid gray",
+                borderRadius: "25px",
+                width: "20px",
+                mr: 1,
+                height: "20px",
+                backgroundColor: "yellow",
+              }
+            : {
+                border: "1px solid gray",
+                borderRadius: "25px",
+                width: "20px",
+                height: "20px",
+                mr: 1,
+              }
+        }
+      ></Box>
+      <Box
+        sx={
+          currentColor === "Green"
+            ? {
+                border: "1px solid gray",
+                borderRadius: "25px",
+                width: "20px",
+                height: "20px",
+                backgroundColor: "green",
+              }
+            : {
+                border: "1px solid gray",
+                borderRadius: "25px",
+                width: "20px",
+                height: "20px",
+              }
+        }
+      ></Box>
     </Box>
   );
 }
